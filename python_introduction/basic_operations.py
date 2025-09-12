fb17000da84a83fb43094065bba01cb87e37250d
yer0san <lebrondee20@gmail.com>
2025-09-12 22:08:36 +0300

    add basic operations task in python introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sumRes = number1 + number2
+differenceRes = number1 - number2
+productRes = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sumRes}")
+print(f"Subtraction of {number1} and {number2} is {differenceRes}")
+print(f"Multiplication of {number1} and {number2} is {productRes}")
